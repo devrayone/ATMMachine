cab0a447b7f48ab2f8723bbd9363237bd52bd34b
Aleksandr Kuzmichev <d5f@ya.ru>
2021-05-13 21:44:33 +0300

    Добавлено демо работы с пропертями (свойствами)

--- src/main/java/ru/devray/study/atmmachine/Launcher.java
+++ src/main/java/ru/devray/study/atmmachine/Launcher.java
@@ -1,10 +1,43 @@
 package ru.devray.study.atmmachine;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class Launcher {
+
+    //local
+    public static String serverUrl;
+    public static String serverPort;
+    public static String dbUrl;
+
+
+    public static void loadProperties(){
+        Properties properties = new Properties();
+
+        try {
+            InputStream is = new FileInputStream("src/main/resources/stand-configuration.properties");
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        serverUrl = properties.getProperty("serverUrl");
+        serverPort = properties.getProperty("serverPort");
+        dbUrl = properties.getProperty("dbUrl");
+
+        System.out.println(serverUrl + " " + serverPort + " " + dbUrl );
+    }
+
     public static void main(String[] args) {
+        //инциализируем переменные конфигурации стенда
+        loadProperties();
+
+
         //пачка купюр, которую пользватель вносит в банкомат
         List<Banknote> money = new ArrayList<Banknote>();
         money.add(new Banknote(Values.FIFTY, true, true));
